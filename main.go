945cdd7bd70de983448162bfdb950bd6e3e6f010
Amanibhavam <iam@defn.sh>
2021-01-06 18:29:11 -0800

    fix main

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
+import "runtime"
 
 func main() {
-    fmt.Println("hello world", version, runtime.GOOS, runtime.GOARCH)
+    fmt.Println("hello world", runtime.GOOS, runtime.GOARCH)
 }

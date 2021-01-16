53e975758c433fe8d300068be516164a6f9b3cff
Amanibhavam <iam@defn.sh>
2021-01-16 12:10:26 -0800

    use apex gateway request test

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
-import "runtime"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/apex/gateway/v2"
+	"github.com/go-chi/chi"
+)
 
 func main() {
-	fmt.Println("hello world", runtime.GOOS, runtime.GOARCH)
+	gateway.ListenAndServe("3000", routing())
+}
+
+func routing() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	})
+
+	r.Get("/id/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	})
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Not found: [%s]", r.RequestURI)))
+	})
+
+	return r
 }
